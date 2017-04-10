e02454e2594c85a4426315b89746b0a7b2f990e0
fanxiaobin <fanxiaobin@liweideMacBook-Air.local>
2017-04-10 14:58:58 +0800

    first init commit

--- KVO/ViewController.m
+++ KVO/ViewController.m
@@ -7,8 +7,25 @@
 //
 
 #import "ViewController.h"
+#import "NSObject+KVO.h"
+
+@interface Message : NSObject
+
+@property (nonatomic, copy) NSString *text;
+
+@end
+
+@implementation Message
+
+@end
 
 @interface ViewController ()
+
+
+@property (nonatomic, weak) IBOutlet UITextField *textfield;
+@property (nonatomic, weak) IBOutlet UIButton *button;
+
+@property (nonatomic, strong) Message *message;
 
 @end
 
@@ -16,9 +33,29 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the
+    
+    
+    self.message = [[Message alloc] init];
+    [self.message PG_addObserver:self forKey:@"text"
+                       withBlock:^(id observedObject, NSString *observedKey, id oldValue, id newValue) {
+                           NSLog(@"%@.%@ is now: %@", observedObject, observedKey, newValue);
+                           dispatch_async(dispatch_get_main_queue(), ^{
+                               self.textfield.text = newValue;
+                           });
+                           
+                       }];
+    
+    [self changeMessage:nil];
 }
 
+
+- (IBAction)changeMessage:(id)sender
+{
+    NSArray *msgs = @[@"Hello World!", @"Objective C", @"Swift", @"Peng Gu", @"peng.gu@me.com", @"www.gupeng.me", @"glowing.com"];
+    NSUInteger index = arc4random_uniform((u_int32_t)msgs.count);
+    self.message.text = msgs[index];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
